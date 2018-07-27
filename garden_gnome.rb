8aea4285978a1c35818fc55bf4afd31ebce9007e
Sara Kim <sarakim0903@gmail.com>
2018-07-27 18:52:10 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -43,15 +43,15 @@ class GardenGnome
   end
 
   def gnaw
-    return "Gnawing on a tree!!!"
+    "Gnawing on a tree!!!"
   end
 
   def shout
-    return "GNARLY!!!"
+    "GNARLY!!!"
   end
 
 
   def introduce_self
-    return "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
+    "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
   end
 end
